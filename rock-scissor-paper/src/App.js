e6f839b5a8799ef968390f8888fb629040d2f009
hojiii <shimn3060@naver.com>
2023-03-01 03:17:54 +0900

    react study first commit

---
+++ rock-scissor-paper/src/App.js
@@ -0,0 +1,48 @@
+import{ useState } from'react';
+import Button from './Button';
+import HandButton from './HandButton';
+import HandIcon from './HandIcon';
+import { compareHand, generateRandomHand } from './utils';
+
+function getResult(me, other) {
+  const comparison = compareHand(me, other);
+  if (comparison > 0) return '승리';
+  if (comparison < 0) return '패배';
+  return '무승부';
+}
+
+
+function App() {
+  const [hand,setHand] = useState('rock')
+  const [otherHand,setOtherHand] = useState('rock')
+
+  const handleButtonClick = (nextHand) => {
+  const nextOtherHand = generateRandomHand();
+  setHand(nextHand);
+  setOtherHand(nextOtherHand);
+  };
+
+  const handleClearClick = () => {
+    setHand('rock');
+    setOtherHand('rock');
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        <HandIcon value={hand} />
+        VS
+        <HandIcon value={otherHand} />
+      </div>
+      <div>
+        <HandButton value="rock" onClick={handleButtonClick} />
+        <HandButton value="scissor" onClick={handleButtonClick} />
+        <HandButton value="paper" onClick={handleButtonClick} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
